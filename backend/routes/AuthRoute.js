33fd44f83e1729d99fdef8ceb64818a3d7519e7d
Arushi <arushigupta98106@gmail.com>
2025-05-25 16:44:41 +0530

    Fixed errors

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -1,4 +1,5 @@
 const { Signup, Login } = require('../controllers/AuthController')
+const { userVerification } = require('../middlewares/AuthMiddleware')
 const router = require('express').Router()
 
 router.post('/signup', Signup)
